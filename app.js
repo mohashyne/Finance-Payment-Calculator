a91749ba0eee9fdd31144c767758cf447c5572e5
Muhammad Aminu Salihu <mohashyne@gmail.com>
2022-08-14 17:05:37 +0100

    functions created

--- app.js
+++ app.js
@@ -16,6 +16,45 @@ function calculateResults(e){
     const calculatedProfit = parseFloat(profit.value) / 100 / 12;
     const calculatedPayments = parseFloat(years.value) * 12; 
 
+    // Compute monthly payment
+    const x = Math.pow(1 + calculatedProfit, calculatedPayments);
+    const monthly = (principal*x*calculatedProfit)/(x-1);
+
+    if(isFinite(monthly)){
+       monthlyPayment.value = monthly.toFixed(2);
+       totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+       totalProfit.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+    }else {
+     showError('Please check your numbers')
+    }
+
     e.preventDefault();
 }
 
+// Show Error
+function showError(error){
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    //Get element
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = ('alert alert-danger');
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+
+
+// Clear error
+function clearError(){
+    document.querySelector('.alert').remove();
+}
